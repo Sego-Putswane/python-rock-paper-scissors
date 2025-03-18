47591c77d3112f4553b350f6737736d799b3539c
leputswjhb024 <leputswjhb024@student.wethinkcode.co.za>
2025-03-18 22:24:57 +0200

    game_initialization() function created. updates made in the get_player_name() function

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,13 +1,29 @@
+import random
+
 #ask user for name and greet them.
 
 def get_player_name():
     global player_name #placing players name under global variable, so it can be accessed in other functions.
-    while True:
-        player_name = input("Enter your name: ").lower()
-        if player_name == "":
-            print("ERROR! player_name can't be empty. Please try again.") #error handling for when name not entered.
-        else:
-            print(f"Welcome to Rock, Paper Scissors, {player_name}")
-            break
+    try:
+        while True:
+            player_name = input("Enter your name: ").lower()
+            if 0 < len(player_name) < 11:
+                print(f"Welcome to Rock, Paper Scissors, {player_name}")
+                break
+    except ValueError:
+        print("ERROR! player_name can't be empty. Please try again.") #error handling for when name not entered or when the name is greater than given limit.
 
-get_player_name()
+def game_initialization():
+    global user_choice
+    global computer_choice
+
+    game_options = ["rock","paper","scissors"]
+    computer_choice = random.choice(game_options)
+
+    while True:
+        user_choice = input("Please choose either rock, paper, or scissors: ").lower()
+        if user_choice not in game_options:
+            print("Invalid input. Please try again.")
+        else:
+            print(f"You chose {user_choice}, and computer chose {computer_choice}.")
+            break
--- test_rock_paper_scissors.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from rock_paper_scissors import get_player_name
-
-class TestRockPaperScissors(unittest.TestCase):
-    def test_get_player_name(self):
-        player_name = "Sego" 
-        self.assertEqual("Welcome to Rock, Paper Scissors, sego")
