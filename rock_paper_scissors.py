ba0a80c6d8e56e0b65956e7f257f76916c932774
leputswjhb024 <leputswjhb024@student.wethinkcode.co.za>
2025-03-18 22:34:48 +0200

    game_initialization() function updated.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,23 +7,24 @@ def get_player_name():
     try:
         while True:
             player_name = input("Enter your name: ").lower()
-            if 0 < len(player_name) < 11:
+            if 0 < len(player_name) < 11: #the condition that will apply if the players name passes all requirements.
                 print(f"Welcome to Rock, Paper Scissors, {player_name}")
                 break
-    except ValueError:
+    except ValueError: # when the players name is invalid 
         print("ERROR! player_name can't be empty. Please try again.") #error handling for when name not entered or when the name is greater than given limit.
 
 def game_initialization():
-    global user_choice
+    global player_choice
     global computer_choice
 
     game_options = ["rock","paper","scissors"]
-    computer_choice = random.choice(game_options)
+    computer_choice = random.choice(game_options) #this is how the computer will be able to make a selection.
 
-    while True:
-        user_choice = input("Please choose either rock, paper, or scissors: ").lower()
-        if user_choice not in game_options:
-            print("Invalid input. Please try again.")
-        else:
-            print(f"You chose {user_choice}, and computer chose {computer_choice}.")
-            break
+    try:
+        while True:
+            player_choice = input("Please choose either rock, paper, or scissors: ").lower() #asking player to make a selection.
+            if player_choice in game_options: #the condition that will apply if the player makes the correct selection.
+                print(f"{player_name} chose {player_choice}, and computer chose {computer_choice}.")
+                break
+    except ValueError: #when the players selection is invalid
+        print("Invalid input. Please try again.")
